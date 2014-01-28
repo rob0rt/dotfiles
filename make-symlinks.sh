86cf9bbd16cbf434d71fa5a1ba4b30215c276988
Robert Lynch <BobertForever@users.noreply.github.com>
2014-01-27 21:45:22 -0600

    Create make-symlinks.sh

---
+++ make-symlinks.sh
@@ -0,0 +1,24 @@
+# bashrc
+if [ ! -L ~/.bashrc ]; then
+  ln -s $PWD/.bashrc ~/
+fi
+
+# bash configs
+if [ ! -L ~/.bash ]; then
+  ln -s $PWD/.bash ~/
+fi
+
+# vimrc
+if [ ! -L ~/.vimrc ]; then
+  ln -s $PWD/.vimrc ~/
+fi
+
+# tmux conf
+if [ ! -L ~/.tmux.conf ]; then
+  ln -s $PWD/.tmux.conf ~/
+fi
+
+# gitconfig
+if [ ! -L ~/.gitconfig ]; then
+  ln -s $PWD/.gitconfig ~/
+fi
