bf0ec1b115c5b3f2d3f3d06776e7f4ac7b354f55
Robert Lynch <BobertForever@users.noreply.github.com>
2014-10-21 10:25:27 -0500

    Added ctrlp to configure

--- configure.sh
+++ configure.sh
@@ -7,5 +7,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # Airline
 git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
 
+# Ctrlp
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+
 # Oh My Zsh
 curl -L http://install.ohmyz.sh | sh
