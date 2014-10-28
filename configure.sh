e980ced0b0376a59110b6da457a40615b35d6dce
Robert Lynch <BobertForever@users.noreply.github.com>
2014-10-27 22:51:10 -0500

    Added Nerdtree to configure

--- configure.sh
+++ configure.sh
@@ -10,5 +10,8 @@ git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
 # Ctrlp
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 
+# Nerdtree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
 # Oh My Zsh
 curl -L http://install.ohmyz.sh | sh
