7f2d18efeadaea1e221f80a47d910695301e6da8
Robert Lynch <BobertForever@users.noreply.github.com>
2015-07-24 15:14:36 +0100

    Add more configs to configure file

--- configure.sh
+++ configure.sh
@@ -16,5 +16,13 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 # Rust
 git clone https://github.com/wting/rust.vim.git ~/.vim/bundle/rust.vim
 
+# Hack
+git clone git://github.com/hhvm/vim-hack.git ~/.vim/bundle/vim-hack
+
+# Vimrc/ Tmux config/ Git config
+wget https://raw.githubusercontent.com/BobertForever/dotfiles/master/.vimrc -P ~/
+wget https://raw.githubusercontent.com/BobertForever/dotfiles/master/.tmux.conf -P ~/
+wget https://raw.githubusercontent.com/BobertForever/dotfiles/master/.gitconfig -P ~/
+
 # Oh My Zsh
 curl -L http://install.ohmyz.sh | sh
