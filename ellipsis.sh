be04e3c1ebe19d980e8f65834c917f859188ae3e
Robert Lynch <robertlynch2012@gmail.com>
2016-03-20 13:11:24 -0700

    Configuration/ zshrc issues

--- ellipsis.sh
+++ ellipsis.sh
@@ -4,15 +4,6 @@
 
 pkg.link() {
     fs.link_files common
-
-    case $(os.platform) in
-        osx)
-            fs.link_files platform/osx
-            ;;
-        linux)
-            fs.link_files platform/linux
-            ;;
-    esac
 }
 
 pkg.install() {
